a4be307f4ba78d01d43993a69da8624595aa9197
cabos <yuto.eguma@gmail.com>
2018-10-17 00:01:22 +0900

    コードの自動生成を抑制

--- src/test/scala/CodeGen.scala
+++ src/test/scala/CodeGen.scala
@@ -8,14 +8,15 @@ class CodeGen extends FunSpec {
   describe("generate slick code") {
 
     it("maihamadb") {
-      slick.codegen.SourceCodeGenerator.main(Array(
-        "slick.jdbc.MySQLProfile",
-        "com.mysql.jdbc.Driver",
-        "jdbc:mysql://localhost:3306/maihamadb",
-        "/Users/yuto/works/slick-sample/src/main/scala/",
-        "slick.mysql",
-        "root",
-        ""))
+      // do noting
+//      slick.codegen.SourceCodeGenerator.main(Array(
+//        "slick.jdbc.MySQLProfile",
+//        "com.mysql.jdbc.Driver",
+//        "jdbc:mysql://localhost:3306/maihamadb",
+//        "/Users/yuto/works/slick-sample/src/main/scala/",
+//        "slick.mysql",
+//        "root",
+//        ""))
     }
   }
 }
